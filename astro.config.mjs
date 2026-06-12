5dc22ee59f883aa3d52992329932a289f74324dc
UrielJafet <jafet200427@hotmail.com>
2026-06-11 20:59:40 -0600

    feat: update astro.config.mjs and package.json for @astrojs/node integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: node({
+    mode: "standalone",
+  }),
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-  output: 'server'
+  output: "server",
 });
